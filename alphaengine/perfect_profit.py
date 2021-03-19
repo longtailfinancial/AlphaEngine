d6e73e766c26a4eab4709b9bbead670b2093e15e
Stuart Farmer <stuart@lamden.io>
2021-03-19 10:55:33 -0700

    core code

--- alphaengine/perfect_profit.py
+++ alphaengine/perfect_profit.py
@@ -51,7 +51,7 @@ def returns_minus_cost(y, outputs, slippage=0.25, contract_cost=2.1, leverage=50
     return returns - costs
 
 
-def train_model(model, optimizer, X, y, window_size=30, epochs=1):
+def train_model_v1(model, optimizer, X, y, window_size=30, epochs=1):
     # Iterate over the batches of a dataset.
     for j in range(epochs):
         print(f'{j+1}/{epochs} epoch...')
@@ -79,3 +79,70 @@ def train_model(model, optimizer, X, y, window_size=30, epochs=1):
 
     return model, outputs, losses
 
+
+def build_windowed_data(data, input_size=50, split=0.75):
+    X_arr = []
+    y_arr = []
+    for i in range(len(data) - input_size):
+        X_arr.append(data[i:i + input_size])
+        y_arr.append(data[i + input_size])
+
+    X = np.array(X_arr)
+    y = np.array(y_arr)
+
+    idx = int(len(X) * split)
+    X_train = X[:idx]
+    y_train = y[:idx]
+
+    X_test = X[idx:]
+    y_test = y[idx:]
+
+    return np.nan_to_num(X_train), np.nan_to_num(y_train), np.nan_to_num(X_test), np.nan_to_num(y_test)
+
+
+def build_batched_data(X, y, batch_size=100):
+    X_arr = []
+    y_arr = []
+    for i in range(len(X) - batch_size):
+        X_arr.append(X[i:i+batch_size])
+        y_arr.append(y[i:i+batch_size])
+
+    X_batches = np.array(X_arr)
+    y_batches = np.expand_dims(np.array(y_arr), axis=-1).astype('float32')
+
+    return X_batches, y_batches
+
+
+def train_model_v3(X_batches, y_batches, model, epochs=10, optimizer=tf.keras.optimizers.Adam()):
+    for j in range(epochs):
+        print(f'{j + 1} / {epochs} epochs...')
+        for i in progressbar.progressbar(range(len(X_batches))):
+            with tf.GradientTape() as tape:
+                logits = model(X_batches[i])
+                loss = tf.cumsum(y_batches[i]) - tf.cumsum(logits * y_batches[i])
+
+                # loss *= tf.cast(tf.range(1, len(y_batches[i]))[::1], tf.float32)
+
+            gradients = tape.gradient(loss, model.trainable_weights)
+            optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+
+        print(f'Loss: {np.average(loss)}')
+
+    return model
+
+
+def train_model_v4(model, X_train, y_train, X_test, y_test, epochs=10, optimizer=tf.keras.optimizers.Adam()):
+    for i in progressbar.progressbar(range(epochs)):
+        with tf.GradientTape() as tape:
+            train_logits = model(X_train)
+            train_loss = tf.cumsum(y_train) - tf.cumsum(train_logits * y_train)
+
+            test_logits = model(X_test)
+            test_loss = tf.cumsum(y_train) - tf.cumsum(test_logits * y_train)
+
+        gradients = tape.gradient(train_loss, model.trainable_weights)
+        optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+
+    print(f'Train loss: {np.average(train_loss)}\nTest loss: {np.average(test_loss)}')
+
+    return model
